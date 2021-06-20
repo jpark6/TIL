a5402a3c3c6aa5a1c31264c9280bc695d9e272ec
Jake Park <qkstjr09@gmail.com>
2021-06-20 21:17:19 +0900

    favicon 수정2

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -17,8 +17,7 @@ module.exports = {
    */
   head: [
     ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "./icon/180.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "./icon/32.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "./icon/16.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "128x128", href: "./icon/128.png"}],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
