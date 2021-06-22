b02f5720466c49f4db831c3862c32180861d5d53
Jake Park <qkstjr09@gmail.com>
2021-06-22 15:48:16 +0900

    sitemap 추가

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -54,10 +54,11 @@ module.exports = {
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-  ],
+  plugins: {
+    'sitemap': {
+      hostname: "https://jpark-til.netlify.app"
+    }
+  },
   markdown: {
     lineNumbers: true
   }
