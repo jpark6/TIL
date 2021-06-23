7ff502dbfe582992536bb999e44f07766345c69d
Jake Park <qkstjr09@gmail.com>
2021-06-23 22:22:07 +0900

    Utterances 오류 수정

--- .vuepress/theme/components/Utterances.vue
+++ .vuepress/theme/components/Utterances.vue
@@ -5,18 +5,21 @@
 <script>
 export default {
   mounted() {
-    const utterances = document.createElement('script');
-    utterances.type = 'text/javascript';
-    utterances.async = true;
-    utterances.crossorigin = 'anonymous';
-    utterances.src = 'https://utteranc.es/client.js';
-    
-    utterances.setAttribute('issue-term', 'title'); // pathname|url|title|og:title 중 택 1
-    utterances.setAttribute('theme','github-light'); // theme 설정
-    utterances.setAttribute('repo',`jpark6/TIL`); // 사용할 repository
+      if (typeof document !== 'undefined' && document.getElementById('contentReply')) {
+        const contentReply = document.getElementById('contentReply')
+        contentReply.innerHTML = ""
+        const utterances = document.createElement('script');
+        utterances.type = 'text/javascript';
+        utterances.async = true;
+        utterances.crossorigin = 'anonymous';
+        utterances.src = 'https://utteranc.es/client.js';
+        
+        utterances.setAttribute('issue-term', 'title'); // pathname|url|title|og:title 중 택 1
+        utterances.setAttribute('theme','github-light'); // theme 설정
+        utterances.setAttribute('repo',`jpark6/TIL`); // 사용할 repository
 
-    // script tag 삽입
-    this.$refs.comment.append(utterances);
+        contentReply.append(utterances)
+      }
   }
 }
 </script>
